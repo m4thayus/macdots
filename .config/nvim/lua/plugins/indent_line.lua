e59f37e0b31d401e32ae6ad5679f19fa3fe47d4c
Matt Williams <matt.seongjun@gmail.com>
2025-12-27 12:49:45 -0500

    feat: configure indent line

--- .config/nvim/lua/plugins/indent_line.lua
+++ .config/nvim/lua/plugins/indent_line.lua
@@ -4,6 +4,49 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = "ibl",
-    opts = {},
+    opts = {
+      indent = {
+        highlight = {
+          "EndOfBuffer",
+          "IblIndent",
+          "IblIndent",
+          "IblIndent",
+          "IblIndent",
+          "IblIndent",
+          "IblIndent",
+        },
+        char = "▏",
+        -- Default: `▎`
+        --
+        -- Alternatives:
+        --   • left aligned solid
+        --     • `▏`
+        --     • `▎` (default)
+        --     • `▍`
+        --     • `▌`
+        --     • `▋`
+        --     • `▊`
+        --     • `▉`
+        --     • `█`
+        --   • center aligned solid
+        --     • `│`
+        --     • `┃`
+        --   • right aligned solid
+        --     • `▕`
+        --     • `▐`
+        --   • center aligned dashed
+        --     • `╎`
+        --     • `╏`
+        --     • `┆`
+        --     • `┇`
+        --     • `┊`
+        --     • `┋`
+        --   • right aligned dotted
+        --     • `⋮`
+        --     • `⁞`
+        --   • center aligned double
+        --     • `║`
+      },
+    },
   },
 }
