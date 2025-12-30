c132ced97988e1bc9d4fb25ad88b4072b232a5e4
Matt Williams <matt.seongjun@gmail.com>
2025-12-30 16:58:01 -0500

    feat: add lsp actions group

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -78,6 +78,7 @@ return {
         { "<leader>s", group = "[S]earch" },
         { "<leader>t", group = "[T]oggle" },
         { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+        { "gr", group = "LSP Actions", mode = { "n" } },
       },
     },
   },
